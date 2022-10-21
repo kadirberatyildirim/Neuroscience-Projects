0604d89a9b78a30560353cffbff3577354cc66b9
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-10-21 18:41:27 +0300

    basic plots of attention are done

--- attention_project/main.m
+++ attention_project/main.m
@@ -1,3 +1,10 @@
 clc;clear;clf;
 
-plot_controller("Box")
+% To make figure fullscreen, uncomment next line!
+figure('units','normalized','outerposition',[0 0 1 1])
+
+for i=1:3
+    clf;
+    plot_controller("Cue")
+    pause(0.5)
+end
--- attention_project/plot_controller.m
+++ attention_project/plot_controller.m
@@ -1,5 +1,25 @@
 function plot_controller(type)
-    if type == "Box"
-        rectangle('Position', [0.2 0.6 0.5 0.2])
+    bounds = 0:4:12; % Defines grids
+    % Choose a random grid
+    point = [randsample(bounds,1) randsample(bounds,1)];
+    % Plot the background grids
+    plot_grids(bounds)
+    hold on
+    if type == "Cue"
+        % Plot the random box
+        f = fill([point(1) point(1) point(1)+4 point(1)+4], ...
+            [point(2) point(2)+4 point(2)+4 point(2)], 'b')
+        alpha(f,0.2)
+    elseif type == "Target"
+        
+    end
+end
+
+% Every plot will contain grids
+function plot_grids(bounds)
+    for j=1:4
+        for i=1:4
+            rectangle('Position', [bounds(i) bounds(j) 4 4])
+        end
     end
 end
