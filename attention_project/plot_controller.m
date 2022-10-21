63d43590984c27d3e150f5893b54effbb608acb8
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-10-21 21:08:08 +0300

    weights implemented

--- attention_project/plot_controller.m
+++ attention_project/plot_controller.m
@@ -1,25 +1,25 @@
-function plot_controller(type)
-    bounds = 0:4:12; % Defines grids
-    % Choose a random grid
-    point = [randsample(bounds,1) randsample(bounds,1)];
+function plot_controller(type, bounds, point)
     % Plot the background grids
     plot_grids(bounds)
     hold on
     if type == "Cue"
         % Plot the random box
-        f = fill([point(1) point(1) point(1)+4 point(1)+4], ...
-            [point(2) point(2)+4 point(2)+4 point(2)], 'b')
+        f = fill([point(1) point(1) point(1)+length(bounds) point(1)+length(bounds)], ...
+            [point(2) point(2)+length(bounds) point(2)+length(bounds) point(2)], 'b');
         alpha(f,0.2)
     elseif type == "Target"
-        
+        % Find middle point of the grid and put a target point
+        s = scatter((2*point(1)+length(bounds))/2, (2*point(2)+length(bounds))/2, ...
+            800, "red", 'filled');
     end
 end
 
 % Every plot will contain grids
 function plot_grids(bounds)
-    for j=1:4
-        for i=1:4
-            rectangle('Position', [bounds(i) bounds(j) 4 4])
+    for j=1:length(bounds)
+        for i=1:length(bounds)
+            rectangle('Position', ...
+                [bounds(i) bounds(j) length(bounds) length(bounds)])
         end
     end
 end
