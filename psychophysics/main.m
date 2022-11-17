39609a2e877b3b5598679a0660dc8494b30fb6b4
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-11-17 16:13:29 +0300

    all trials implemented and code is ready to use

--- psychophysics/main.m
+++ psychophysics/main.m
@@ -1,20 +1,50 @@
 clc; clear; clf;
 
-trials = ['Fovial' 'Parafovial' 'RGB' 'Relative'];
+scores = ["Trial" "brightness-"+string(0:10:90)]; % List that will store all trial results
 
-pause_figure("Press enter to continue trials")
+h = figure('units','normalized','outerposition',[0 0 1 1]) %Creating a figure with a handle h
+set(h,'color',[0 0 0]) % Makes the figure window black
 
-s = trial
+%%%%% Starting Screen %%%%%
+start_text = "Press enter to continue trials";
+pause_figure(start_text)
 
+%%%%% Para/fovial experiment with shades of gray %%%%%
+dists = 0:40:200; % Distances to the right from the center
+for dist = dists
+    pause_text = "You will be starting phase with distance " + string(dist) + ". Focus on the center.";
+    pause_figure(pause_text)
+    s = trial(h, dist, [1 2 3], 0); % [1 2 3] is the gray color
+    scores = [scores; "Gray-"+string(dist) transpose(s)];
+end
 
+%%%%% RGB experiment %%%%%
+colours = ["R" "G" "B"];
+for i = 1:3 % In order, R, G and B trials are done this way
+    for j = 3:4
+        pause_text = "You will be starting trial with color " + colours(i) + " and with distance " + string(dists(j)) + ". Focus on the center.";
+        pause_figure(pause_text)
+        s = trial(h, dists(j), i, 0);
+        scores = [scores; colours(i)+"-"+string(dists(j)) transpose(s)];
+    end
+end
 
+%%%%% Relative experiment %%%%%
+for i = 2:3
+    pause_text = "You will be starting relative trial with distance " + string(dist) + ". Focus on the center and decide if right dot is dimmer or brighter than the left dot.";
+    pause_figure(pause_text)
+    s = trial(h, dists(i), [1 2 3], dists(i));
+    scores = [scores; colours(i)+"-"+string(dists(j)) transpose(s)];
+end
 
-
+% Save trial data
+writematrix(scores, string(datetime()) + "_session_data.csv")
 
 %% Helpers
 function pause_figure(txt)
     clf;
-    text (0.3, 0.5, txt);
+    image(uint8(zeros(400,400,3)))
+    text(50, 200, txt, 'Color', 'r');
     % When enter pressed, this while will end
     inp = get_input;
     while inp ~= 13
--- psychophysics/trial.m
+++ psychophysics/trial.m
@@ -1,13 +1,14 @@
-function score = trial(color)
+function score = trial(h, dist, color, rel)
     temp = uint8(zeros(400,400,3)); %Create a dark stimulus matrix
     temp1 = cell(10,1); %Create a cell that can hold 10 matrices
     for ii = 1:10 %Filling temp1
-        temp(200,200,:) = 255; %Inserting a fixation point
-        temp(200,240,:) = (ii-1)*10; %Inserting a test point 40 pixels right of it. Brightness range 0 to 90.
+        if dist ~= 0; temp(200,200,:) = 255; end %Inserting a fixation point
+        temp(200,200+dist,color) = (ii-1)*10; %Inserting a test point dist pixels right of it. Brightness range 0 to 90.
+        if rel ~= 0; temp(200,200-dist,color) = rel; end
         temp1{ii} = temp; %Putting the respective modified matrix in cell
     end %Done doing that
-    h = figure('units','normalized','outerposition',[0 0 1 1]) %Creating a figure with a handle h
-    set(h,'color',[0 0 0]) % Makes the figure window black
+    %h = figure('units','normalized','outerposition',[0 0 1 1]) %Creating a figure with a handle h
+    %set(h,'color',[0 0 0]) % Makes the figure window black
     stimulusorder = randperm(200); %Creating a random order from 1 to 200.
     %For the 200 trials. Allows to have a precisely equal number per condition.
     stimulusorder = mod(stimulusorder,10); %Using the modulus function to create a range from 0 to 9. 20 each.
@@ -15,7 +16,7 @@ function score = trial(color)
     score = zeros(10,1); %Keeping score. How many stimuli were reported seen
     for ii = 1:200 %200 trials, 20 per condition
         image(temp1{stimulusorder(1,ii)}) %Image the respective matrix. As designated by stimulusorder
-        ii %Give observer feedback about which trial we are in. No other feedback.
+        %ii %Give observer feedback about which trial we are in. No other feedback.
         pause; %Get the keypress
         temp2 = get(h,'CurrentCharacter'); %Get the keypress. =." for present, “,” for absent.
         temp3 = strcmp('.', temp2); %Compare strings. If . (present), temp3 = 1, otherwise 0.
