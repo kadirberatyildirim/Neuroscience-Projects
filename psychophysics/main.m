deaff18c469eae8e30a58f215d00deb7f9343fdb
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-11-18 00:43:50 +0300

    bug fix for relative trials

--- psychophysics/main.m
+++ psychophysics/main.m
@@ -35,7 +35,7 @@ for i = 2:3
         pause_text = "You will be starting relative trial with distance " + string(dist) + ". Focus on the center and decide if right dot is dimmer or brighter than the left dot.";
         pause_figure(pause_text)
         s = trial(h, dists(i), [1 2 3], b);
-        scores = [scores; colours(i)+"-"+string(dists(j))+"-"+string(b) transpose(s)];
+        scores = [scores; "R-"+string(dists(i))+"-"+string(b) transpose(s)];
     end
 end
 
